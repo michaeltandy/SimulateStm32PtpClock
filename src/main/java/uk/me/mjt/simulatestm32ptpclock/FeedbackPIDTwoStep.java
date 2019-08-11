41d440a9a1ecb0223feedab6c47e740ee71bd512
mtandy <mtandy@mtandy-desktop>
2019-08-11 10:17:03 +0100

    Two-step PID controller with 6 input parameters.

--- src/main/java/uk/me/mjt/simulatestm32ptpclock/FeedbackPIDTwoStep.java
+++ src/main/java/uk/me/mjt/simulatestm32ptpclock/FeedbackPIDTwoStep.java
@@ -6,21 +6,27 @@ import java.math.BigInteger;
 
 
 public class FeedbackPIDTwoStep implements FeedbackController {
-    private final double proportionalGain;
-    private final double integralGain;
-    private final double derivativeGain;
+    private final double proportionalGain1;
+    private final double integralGain1;
+    private final double derivativeGain1;
+    private final double proportionalGain2;
+    private final double integralGain2;
+    private final double derivativeGain2;
     
     private long lastErrorMicros = Long.MAX_VALUE;
     
-    private int errorIntegral = 0;
+    private long errorIntegral = 0;
     boolean passedStep = false;
     
     String lastNote;
     
-    public FeedbackPIDTwoStep(double p, double i, double d) {
-        this.proportionalGain = p;
-        this.integralGain = i;
-        this.derivativeGain = d;
+    public FeedbackPIDTwoStep(double p1, double i1, double d1, double p2, double i2, double d2) {
+        this.proportionalGain1 = p1;
+        this.integralGain1 = i1;
+        this.derivativeGain1 = d1;
+        this.proportionalGain2 = p2;
+        this.integralGain2 = i2;
+        this.derivativeGain2 = d2;
     }
     
     @Override
@@ -28,44 +34,32 @@ public class FeedbackPIDTwoStep implements FeedbackController {
         long trueTimeMicros = trueTimeSeconds.multiply(new BigDecimal("1000000")).longValueExact();
         long errorMicros = measuredTimeNanoseconds.divide(new BigInteger("1000")).longValueExact()-trueTimeMicros;
         
-        double pid;
+        double p = 0;
+        double i = 0;
+        double d = 0;
         
         if (lastErrorMicros != Long.MAX_VALUE) {
             long derivativeTerm = errorMicros-lastErrorMicros;
             errorIntegral += errorMicros;
             
-            if (trueTimeMicros > 10*60*1000000) {
-                /*if (passedStep != true) {
-                    passedStep = true;
-                    errorIntegral = errorIntegral * 30;
-                }*/
-                
-                pid = proportionalGain/5*errorMicros +
-                        integralGain*errorIntegral;
-                
-                lastNote = String.format("%.4f\t%.4f\t%.4f",
-                    (proportionalGain/5*errorMicros),
-                    (integralGain*errorIntegral),
-                    0.0);
-                
+            if (trueTimeMicros < 10*60*1000000) {
+                p = proportionalGain1*errorMicros;
+                i = integralGain1*errorIntegral;
+                d = derivativeGain1*derivativeTerm;
             } else {
-            
-                pid = proportionalGain*errorMicros
-                        +integralGain*errorIntegral
-                        +derivativeGain*derivativeTerm;
-                
-                lastNote = String.format("%.4f\t%.4f\t%.4f",
-                    (proportionalGain*errorMicros),
-                    (integralGain*errorIntegral),
-                    (derivativeGain*derivativeTerm));
+                if (passedStep == false) {
+                    passedStep = true;
+                    errorIntegral = Math.round((errorIntegral*integralGain1)/integralGain2);
+                }
+                p = proportionalGain2*errorMicros;
+                i = integralGain2*errorIntegral;
+                d = derivativeGain2*derivativeTerm;
             }
-            
-            return new BigInteger(""+Math.round(pid));
         }
         
         lastErrorMicros = errorMicros;
-        
-        return BigInteger.ZERO;
+        lastNote = String.format("%.4f\t%.4f\t%.4f",p,i,d);
+        return new BigInteger(""+Math.round(p+i+d));
     }
     
     public String getNote() {
